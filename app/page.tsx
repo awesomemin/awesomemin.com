8c9eac3b617c79b72f3f7afb85b3221f80b77f1b
Seongmin Oh <saogig0503@naver.com>
2026-01-13 10:35:12 +0900

    design: fix site title and introduction phrase

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LightRays from '@/components/LightRays';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col px-4">
       <div className="w-full h-dvh border-b border-b-muted relative">
         <LightRays
           raysOrigin="top-center"
@@ -18,14 +18,17 @@ export default function Home() {
           noiseAmount={0.05}
           distortion={0}
         />
-        <div className="text-center absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="text-8xl font-black">Awesomemin</h1>
-          <p className="text-2xl">
+        <div className="w-full items-center flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="text-4xl sm:text-8xl font-black text-center bg-linear-to-t from-zinc-600 to-white bg-clip-text text-transparent">
+            Awesomemin
+          </h1>
+          <p className="text-lg sm:text-2xl max-w-5xl">
             <span className="font-bold">
-              안녕하세요, 개발자 겸 예비창업자 오성민입니다.
+              안녕하세요, 개발자 겸 예비창업가 오성민입니다.
             </span>{' '}
-            기술 스택에 구애받지 않고 그때 그때 재밌어 보이는 것들 위주로
-            공부하고 만들어보고 있습니다.
+            분야와 기술 스택을 막론하고 재밌어 보이는 것은 무엇이든 공부하고
+            만들어보고 있습니다. 강한 실행력과 어려운 문제를 물고 늘어지는
+            성향이 강점입니다.
           </p>
         </div>
       </div>
